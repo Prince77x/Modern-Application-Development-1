8b4ed2655ce5e0199a7b26f798b447a7a869f3bf
Prince kumar <24f2005919@ds.study.iitm.ac.in>
2026-01-03 11:40:48 +0530

    updating flask app1

--- 4. Flask web framework/Flask app 1/app.py
+++ 4. Flask web framework/Flask app 1/app.py
@@ -1,10 +1,23 @@
-from flask import Flask 
+from flask import Flask,render_template, request
 app = Flask(__name__) # initializing the flask app
 
 # routes 
 @app.route('/')
 def home():
     return "welcome to your first flask app"
+
+@app.route('/register', methods=["GET","POST"])
+def register():
+    if request.method == "POST":            # request.args is used to read data from url if the method is GET
+        username = request.form["username"] # request.form is used to read data from url if the method is POST 
+        pswd = request.form["pswd"]
+        print(f"{username} , {pswd}")
+        print(request.method) # request.method will print the method name what we are using 
+        return f'Your name is :{username} and password is : {pswd}' 
+        #return render_template('./home.html')
+        
+
+    return render_template('./register.html')
 
 
 
@@ -16,4 +29,4 @@ def home():
 
 # execution of flask app
 if __name__ == "__main__":
-    app.run(debub=True)
+    app.run(debug=True)
